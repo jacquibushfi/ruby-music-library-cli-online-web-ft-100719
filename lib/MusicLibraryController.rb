fa75a72ddb2cc3b0c70be5e16f087472746cc9a9
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-25 16:27:44 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -77,17 +77,28 @@ class MusicLibraryController
     end
 
     def list_songs_by_genre
-    puts "Please enter the name of a genre:"
-    user_input = gets.chomp
-    genre_songs = []
-    self.library.each do |song|
-      if song.genre.name == user_input
-        genre_songs << song
+      puts "Please enter the name of a genre:"
+      user_input = gets.chomp
+      genre_songs = []
+      self.library.each do |song|
+        if song.genre.name == user_input
+          genre_songs << song
+        end
       end
-    end
-      genre_songs = genre_songs.sort_by{|song|song.name}
-      genre_songs.each {|song|puts "#{genre_songs.index(song) + 1}. #{song.artist.name} - #{song.name}"} unless genre_songs == nil
+        genre_songs = genre_songs.sort_by{|song|song.name}
+        genre_songs.each {|song|puts "#{genre_songs.index(song) + 1}. #{song.artist.name} - #{song.name}"} unless genre_songs == nil
   end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    song_names = self.song_array
+    user_input = gets.chomp.to_i
+    if user_input > 0 && user_input <= self.library.size
+      chosen_input = song_names[user_input - 1]
+      chosen_input = name_extractor(chosen_input)[1]
+      song = Song.find_by_name(chosen_input)
+      puts "Playing #{song.name} by #{song.artist.name}" unless song == nil
+    end
 
 
 
